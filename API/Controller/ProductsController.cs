311ac391ee7f92083063cb6a1973cf03af015cee
mustafaguler3 <mustafa.glr@hotmail.com>
2022-10-31 11:56:45 +0300

    initial commit

---
+++ API/Controller/ProductsController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controller
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+    }
+}
